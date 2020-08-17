9496a2b5432933a1e2eabb4b9ee2d38928b8cc3c
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-17 19:09:54 +1000

    Add files via upload

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,13 @@
+#Best Time to Buy and Sell Stock
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit, min_price = 0, float("inf")
+        for price in prices:
+            min_price = min(price, min_price)
+            max_profit = max(max_profit, price-min_price)
+        return max_profit
